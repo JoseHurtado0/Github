12b3e0a46eb64b859d245f3cbc59f7966e8e0961
JoseHurtado0 <josegary0@gmail.com>
2026-05-18 19:50:17 +0200

    Mi primer commit

--- src/git/Prueba.java
+++ src/git/Prueba.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Prueba {
 
 	public static void main(String[] args) {
-		Scanner sn = new Scanner(System.in);
-		System.out.println("Como te llamas?");
-		String nombre = sn.next();
-		System.out.println("Cual es tu apellido?");
-		String apellido = sn.next();
-		
-		System.out.println("------------------");
-		System.out.println("Pues el Don " + nombre + " " + apellido + " es gay");
-		
+		try (Scanner sn = new Scanner(System.in)) {
+			System.out.println("Como te llamas?");
+			String nombre = sn.next();
+			System.out.println("Cual es tu apellido?");
+			String apellido = sn.next();
+			
+			System.out.println("------------------");
+			System.out.println("Pues el Don " + nombre + " " + apellido + " es gay");
+			
+			
+			sn.close();
+		}
 
 	}
 
