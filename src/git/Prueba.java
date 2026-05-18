c1671a8d09e9beba0e97e405a2a91a01b57f96ab
JoseHurtado0 <josegary0@gmail.com>
2026-05-18 19:44:13 +0200

    Mis primeras practicas

--- src/git/Prueba.java
+++ src/git/Prueba.java
@@ -1,9 +1,19 @@
 package git;
 
+import java.util.Scanner;
+
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sn = new Scanner(System.in);
+		System.out.println("Como te llamas?");
+		String nombre = sn.next();
+		System.out.println("Cual es tu apellido?");
+		String apellido = sn.next();
+		
+		System.out.println("------------------");
+		System.out.println("Pues el Don " + nombre + " " + apellido + " es gay");
+		
 
 	}
 
